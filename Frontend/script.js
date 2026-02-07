0b9f5d170bbb6b3cdd7ae04b7a42cd7dc3077134
manishcodes-dot <manishrana98449@gmail.com>
2026-02-07 15:07:15 +0545

    finished the layout

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,9 +1,9 @@
 
 
-function learn{
-    window.alert("I am learning JavaScript!");
-}
+// function learn{
+//     window.alert("I am learning JavaScript!");
+// }
 
-learn(); 
+// learn(); 
+// <button onclick={() => {console.log("i am learning")}}>Click me to learn JavaScript</button>
 
-<button onclick={() => {console.log("i am learning")}}>Click me to learn JavaScript</button>
